72e201df2f2f233d43de8f6f2826729ef9a833ae
nyiyui <+@nyiyui.ca>
2025-01-11 17:26:25 -0500

    oops

--- cmd/device-client/main.go
+++ cmd/device-client/main.go
@@ -209,9 +209,7 @@ func createGoroutines(m *MachineData, dnsClient dns.Client, config Config) {
 			if err != nil {
 				panic(err)
 			}
-			if !config.CanForward {
-				c.SetCanForward(false)
-			}
+			c.SetCanForward(config.CanForward)
 			c.Machine = m.Machines[clientName]
 			c.SetDNSClient(dnsClient)
 			continuous := new(device.ContinousClient)
