b76ece6a98e45c80a2ba47ed67b987c01f44b170
nyiyui <+@nyiyui.ca>
2025-01-11 18:00:29 -0500

    oops

--- goal/apply_linux.go
+++ goal/apply_linux.go
@@ -61,7 +61,7 @@ func ApplyMachineDiff(a, b Machine, md MachineDiff, client *wgctrl.Client, handl
 		if err != nil {
 			return fmt.Errorf("reading /proc/sys/net/ipv4/ip_forward: %w", err)
 		}
-		value, err := strconv.Atoi(bytes.TrimSpace(data))
+		value, err := strconv.Atoi(string(bytes.TrimSpace(data)))
 		if err != nil {
 			return fmt.Errorf("parsing /proc/sys/net/ipv4/ip_forward: %w", err)
 		}
